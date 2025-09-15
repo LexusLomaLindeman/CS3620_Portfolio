267bfbd2fb325b4b9537fe2e34f6c66570d59b9c
Lexus Loma Lindeman <138722437+LexusLomaLindeman@users.noreply.github.com>
2025-09-15 02:01:12 -0600

    Started Challenge 5
    Finished home and contact working on cards for hobbies

---
+++ Challenge_5/PortfolioDatabase/Hobbies/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Hobbie, Project
+
+admin.site.register(Hobbie)
+admin.site.register(Project)
---
+++ Challenge_5/PortfolioDatabase/Hobbies/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Hobbie(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=500)
+    image = models.ImageField(upload_to='image/hobbies')
+
+    def __str__(self):
+        return(f"ID: {self.id} Hobbie Name:{ self.name} Hobbie Description: {self.description}")
+
+class Project(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=500)
+    image = models.ImageField(upload_to='image/projects/')
+    
+    def __str__(self):
+        return(f"ID:{self.id} Project Name:{self.name} Project Description: {self.description}")
