5799e697f7eca08758f00b69ddcf447dee94543c
Lexus Loma Lindeman <138722437+LexusLomaLindeman@users.noreply.github.com>
2025-09-15 02:45:25 -0600

    card image
    Images for hobbie now work

--- Challenge_5/PortfolioDatabase/Hobbies/models.py
+++ Challenge_5/PortfolioDatabase/Hobbies/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Hobbie(models.Model):
     name = models.CharField(max_length=64)
     description = models.CharField(max_length=500)
-    image = models.ImageField(upload_to='image/hobbies')
+    image = models.ImageField(blank=True)
 
     def __str__(self):
         return(f"ID: {self.id} Hobbie Name:{ self.name} Hobbie Description: {self.description}")
@@ -12,7 +12,7 @@ class Hobbie(models.Model):
 class Project(models.Model):
     name = models.CharField(max_length=64)
     description = models.CharField(max_length=500)
-    image = models.ImageField(upload_to='image/projects/')
+    image = models.ImageField(blank=True)
     
     def __str__(self):
         return(f"ID:{self.id} Project Name:{self.name} Project Description: {self.description}")
